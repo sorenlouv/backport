3fec8e5702a8d85a0a6e247a4ec5ace7e9d39e66
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-04-29 00:17:06 +0200

    Improve tests

--- test/steps/doBackportVersions.test.ts
+++ test/steps/doBackportVersions.test.ts
@@ -18,87 +18,91 @@ describe('doBackportVersion', () => {
       .reply(200, {});
   });
 
-  it('with pull request reference', async () => {
-    const createPRMock = nock('https://api.github.com')
-      .post(`/repos/elastic/kibana/pulls`, {
-        title: '[6.x] myCommitMessage | myOtherCommitMessage',
-        body:
-          'Backports the following commits to 6.x:\n - myCommitMessage (#1000)\n - myOtherCommitMessage (#2000)',
-        head: 'sqren:backport/6.x/pr-1000_pr-2000',
-        base: '6.x'
-      })
-      .query(true)
-      .reply(200, {
-        number: 1337,
-        html_url: 'myHtmlUrl'
-      });
+  describe('when commit has a pull request reference', () => {
+    it('should create PR with PR reference', async () => {
+      const createPRMock = nock('https://api.github.com')
+        .post(`/repos/elastic/kibana/pulls`, {
+          title: '[6.x] myCommitMessage | myOtherCommitMessage',
+          body:
+            'Backports the following commits to 6.x:\n - myCommitMessage (#1000)\n - myOtherCommitMessage (#2000)',
+          head: 'sqren:backport/6.x/pr-1000_pr-2000',
+          base: '6.x'
+        })
+        .query(true)
+        .reply(200, {
+          number: 1337,
+          html_url: 'myHtmlUrl'
+        });
 
-    const commits = [
-      {
-        sha: 'mySha',
-        message: 'myCommitMessage',
-        pullNumber: 1000
-      },
-      {
-        sha: 'mySha2',
-        message: 'myOtherCommitMessage',
-        pullNumber: 2000
-      }
-    ];
+      const commits = [
+        {
+          sha: 'mySha',
+          message: 'myCommitMessage',
+          pullNumber: 1000
+        },
+        {
+          sha: 'mySha2',
+          message: 'myOtherCommitMessage',
+          pullNumber: 2000
+        }
+      ];
 
-    const execSpy = jest.spyOn(childProcess, 'exec');
+      const execSpy = jest.spyOn(childProcess, 'exec');
 
-    const res = await doBackportVersion(
-      'elastic',
-      'kibana',
-      commits,
-      '6.x',
-      'sqren',
-      ['backport']
-    );
+      const res = await doBackportVersion(
+        'elastic',
+        'kibana',
+        commits,
+        '6.x',
+        'sqren',
+        ['backport']
+      );
 
-    expect(res).toMatchSnapshot();
-    expect(execSpy.mock.calls).toMatchSnapshot();
-    expect(createPRMock.isDone()).toBe(true);
-    expect(addLabelMock.isDone()).toBe(true);
+      expect(res).toMatchSnapshot();
+      expect(execSpy.mock.calls).toMatchSnapshot();
+      expect(createPRMock.isDone()).toBe(true);
+      expect(addLabelMock.isDone()).toBe(true);
+    });
   });
 
-  it('without pull request reference', async () => {
-    const createPRMock = nock('https://api.github.com')
-      .post(`/repos/elastic/kibana/pulls`, {
-        title: '[6.x] myCommitMessage',
-        body:
-          'Backports the following commits to 6.x:\n - myCommitMessage (mySha)',
-        head: 'sqren:backport/6.x/commit-mySha',
-        base: '6.x'
-      })
-      .query(true)
-      .reply(200, {
-        number: 1337,
-        html_url: 'myHtmlUrl'
-      });
+  describe('when commit does not have a pull request reference', () => {
+    it('should create PR correctly without PR reference', async () => {
+      const createPRMock = nock('https://api.github.com')
+        .post(`/repos/elastic/kibana/pulls`, {
+          title: '[6.x] myCommitMessage',
+          body:
+            'Backports the following commits to 6.x:\n - myCommitMessage (mySha)',
+          head: 'sqren:backport/6.x/commit-mySha',
+          base: '6.x'
+        })
+        .query(true)
+        .reply(200, {
+          number: 1337,
+          html_url: 'myHtmlUrl'
+        });
 
-    const commits = [
-      {
-        sha: 'mySha',
-        message: 'myCommitMessage'
-      }
-    ];
+      const commits = [
+        {
+          sha: 'mySha',
+          message: 'myCommitMessage'
+        }
+      ];
 
-    const execSpy = jest.spyOn(childProcess, 'exec');
+      const execSpy = jest.spyOn(childProcess, 'exec');
 
-    const res = await doBackportVersion(
-      'elastic',
-      'kibana',
-      commits,
-      '6.x',
-      'sqren'
-    );
+      const res = await doBackportVersion(
+        'elastic',
+        'kibana',
+        commits,
+        '6.x',
+        'sqren'
+      );
 
-    expect(res).toMatchSnapshot();
-    expect(execSpy.mock.calls).toMatchSnapshot();
-    expect(createPRMock.isDone()).toBe(true);
-    expect(addLabelMock.isDone()).toBe(false);
+      expect(res).toMatchSnapshot();
+      expect(execSpy.mock.calls).toMatchSnapshot();
+      expect(createPRMock.isDone()).toBe(true);
+      expect(addLabelMock.isDone()).toBe(false);
+    });
   });
 });
 
