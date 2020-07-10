7333b56513bcc8d7b24104196233ee7cac0fb8ca
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-07-10 23:28:22 +0200

    Bump deps

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
 import { runWithArgs } from './runWithArgs';
 const args = process.argv.slice(2);
 
+// this is the entrypoint when running from command line
 runWithArgs(args);
--- src/runAsModule.ts
+++ src/runAsModule.ts
@@ -2,6 +2,8 @@ import { BackportOptions } from './options/options';
 import { runWithArgs } from './runWithArgs';
 const args = process.argv.slice(2);
 
+// this is the entry point when importing backport as module:
+// `import { run } from `backport`
 export function run(options: Partial<BackportOptions>) {
   return runWithArgs(args, options);
 }
