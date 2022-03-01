093c750caf137a46cec9b794f58668bd79692d79
Søren Louv-Jansen <sorenlouv@gmail.com>
2022-03-01 23:52:39 +0100

    Cleanup

--- src/backportRun.ts
+++ src/backportRun.ts
@@ -109,31 +109,48 @@ export async function backportRun(
       });
     }
 
-    if (e instanceof HandledError || e instanceof GithubV4Exception) {
-      if (!options?.ls) {
-        consoleLog(e.message);
-      }
-    } else if (e instanceof Error) {
-      // output
-      consoleLog('\n');
-      consoleLog(chalk.bold('⚠️  Ouch! An unhandled error occured 😿'));
-      consoleLog(e.stack ? e.stack : e.message);
-      consoleLog(
-        'Please open an issue in https://github.com/sqren/backport/issues or contact me directly on https://twitter.com/sorenlouv'
-      );
-
-      consoleLog(
-        chalk.italic(
-          `For additional details see the logs: ${getLogfilePath({
-            logFilePath,
-          })}`
-        )
-      );
-    }
+    outputError({ e, logFilePath, ls: options?.ls });
 
     logger.error('Unhandled exception', e);
     process.exitCode = 1;
 
     return backportResponse;
+  }
+}
+
+function outputError({
+  e,
+  logFilePath,
+  ls,
+}: {
+  e: HandledError | GithubV4Exception<any> | Error;
+  logFilePath?: string;
+  ls?: boolean;
+}) {
+  if (ls) {
+    return;
+  }
+
+  if (e instanceof HandledError || e instanceof GithubV4Exception) {
+    consoleLog(e.message);
+    return;
+  }
+
+  if (e instanceof Error) {
+    // output
+    consoleLog('\n');
+    consoleLog(chalk.bold('⚠️  Ouch! An unhandled error occured 😿'));
+    consoleLog(e.stack ? e.stack : e.message);
+    consoleLog(
+      'Please open an issue in https://github.com/sqren/backport/issues or contact me directly on https://twitter.com/sorenlouv'
+    );
+
+    consoleLog(
+      chalk.italic(
+        `For additional details see the logs: ${getLogfilePath({
+          logFilePath,
+        })}`
+      )
+    );
   }
 }
