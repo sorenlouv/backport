b5377122dc8dfbae862326587c3f1f0445adf715
Søren Louv-Jansen <sorenlouv@gmail.com>
2022-06-08 01:38:22 +0200

    Improve test

--- src/test/e2e/cli/repo-with-changing-branchLabelMapping.private.test.ts
+++ src/test/e2e/cli/repo-with-changing-branchLabelMapping.private.test.ts
@@ -7,72 +7,111 @@ jest.setTimeout(15_000);
 
 describe('backport-org/repo-with-changing-branchLabelMapping', () => {
   describe('for commit merged before branchLabelMapping was changed', () => {
-    let commit: Commit;
-    beforeAll(async () => {
-      const { output } = await runBackportViaCli([
-        '--repo=backport-org/repo-with-changing-branchLabelMapping',
-        '--no-fork',
-        `--accessToken=${accessToken}`,
-        '--json',
-        '--ls',
-      ]);
+    describe('CLI', () => {
+      let commit: Commit;
+      beforeAll(async () => {
+        const { output } = await runBackportViaCli([
+          '--repo=backport-org/repo-with-changing-branchLabelMapping',
+          '--no-fork',
+          `--accessToken=${accessToken}`,
+          '--json',
+          '--ls',
+        ]);
 
-      commit = JSON.parse(output).commits[1];
+        commit = JSON.parse(output).commits[2];
+      });
+
+      it('suggests 3 target branches', async () => {
+        expect(commit.suggestedTargetBranches).toEqual([
+          '8.3',
+          '8.2',
+          'production',
+        ]);
+      });
+
+      it('should shows 2 target branches as NOT_CREATED', () => {
+        expect(
+          commit.pullRequestStates.map(({ branch, label, state }) => ({
+            branch,
+            label,
+            state,
+          }))
+        ).toEqual([
+          { branch: '8.3', label: 'v8.3.0', state: 'NOT_CREATED' },
+          { branch: '8.2', label: 'v8.2.0', state: 'NOT_CREATED' },
+        ]);
+      });
     });
 
-    it('suggests 3 target branches', async () => {
-      expect(commit.suggestedTargetBranches).toEqual([
-        '8.3',
-        '8.2',
-        'production',
-      ]);
-    });
+    describe('JSON', () => {
+      it('displays commit correctly', async () => {
+        const { output } = await runBackportViaCli(
+          [
+            '--repo=backport-org/repo-with-changing-branchLabelMapping',
+            '--no-fork',
+            `--accessToken=${accessToken}`,
+          ],
+          { waitForString: 'Select commit' }
+        );
 
-    it('should shows 2 target branches as NOT_CREATED', () => {
-      expect(
-        commit.pullRequestStates.map(({ branch, label, state }) => ({
-          branch,
-          label,
-          state,
-        }))
-      ).toEqual([
-        { branch: '8.3', label: 'v8.3.0', state: 'NOT_CREATED' },
-        { branch: '8.2', label: 'v8.2.0', state: 'NOT_CREATED' },
-      ]);
+        expect(output).toContain('Create denmark.txt 8.3, 8.2');
+      });
+
+      it('displays pre-selects branches correctly', async () => {
+        const { output } = await runBackportViaCli(
+          [
+            '--repo=backport-org/repo-with-changing-branchLabelMapping',
+            '--no-fork',
+            '--pr=6',
+            `--accessToken=${accessToken}`,
+          ],
+          { waitForString: 'Select branch' }
+        );
+
+        expect(output).toMatchInlineSnapshot(`
+    "? Select branch (Press <space> to select, <a> to toggle all, <i> to invert 
+    selection, and <enter> to proceed)
+    ❯◯ 8.4
+     ◉ 8.3
+     ◉ 8.2
+     ◉ production"
+  `);
+      });
     });
   });
 
-  it('displays commit correctly', async () => {
-    const { output } = await runBackportViaCli(
-      [
-        '--repo=backport-org/repo-with-changing-branchLabelMapping',
-        '--no-fork',
-        `--accessToken=${accessToken}`,
-      ],
-      { waitForString: 'Select commit' }
-    );
+  describe('for commit merged after branchLabelMapping was changed', () => {
+    describe('CLI', () => {
+      let commit: Commit;
+      beforeAll(async () => {
+        const { output } = await runBackportViaCli([
+          '--repo=backport-org/repo-with-changing-branchLabelMapping',
+          '--no-fork',
+          `--accessToken=${accessToken}`,
+          '--json',
+          '--ls',
+        ]);
 
-    expect(output).toContain('Create denmark.txt 8.3, 8.2');
-  });
+        commit = JSON.parse(output).commits[0];
+      });
 
-  it('displays pre-selects branches correctly', async () => {
-    const { output } = await runBackportViaCli(
-      [
-        '--repo=backport-org/repo-with-changing-branchLabelMapping',
-        '--no-fork',
-        '--pr=6',
-        `--accessToken=${accessToken}`,
-      ],
-      { waitForString: 'Select branch' }
-    );
+      it('suggests "production" as target branch', async () => {
+        expect(commit.suggestedTargetBranches).toContain('production');
+      });
 
-    expect(output).toMatchInlineSnapshot(`
-      "? Select branch (Press <space> to select, <a> to toggle all, <i> to invert 
-      selection, and <enter> to proceed)
-      ❯◯ 8.4
-       ◉ 8.3
-       ◉ 8.2
-       ◉ production"
-    `);
+      it('shows "production" as NOT_CREATED', () => {
+        expect(
+          commit.pullRequestStates.map(({ branch, label, state }) => ({
+            branch,
+            label,
+            state,
+          }))
+        ).toContainEqual({
+          branch: 'production',
+          label: 'backport-to-production',
+          state: 'NOT_CREATED',
+        });
+      });
+    });
   });
 });
