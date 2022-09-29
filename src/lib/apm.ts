aad68a8c26db916284552c4c44b61a5f66ee6108
Søren Louv-Jansen <sorenlouv@gmail.com>
2022-09-29 14:25:11 +0200

    Add apm config

--- src/lib/apm.ts
+++ src/lib/apm.ts
@@ -1,11 +1,17 @@
 import apm from 'elastic-apm-node';
+import { accessTokenReplacer } from './logger';
 
-apm.start({
-  serviceName: 'backport',
-  secretToken: '',
-  apiKey: '',
-  serverUrl: '',
-  logLevel: 'off',
-});
-
-export { apm };
+apm
+  .start({
+    serviceName: 'backport',
+    secretToken: 'GZui3tX4jFYjszDweu',
+    serverUrl:
+      'https://f27ab01db9584e008c443b7137d16425.apm.europe-west2.gcp.elastic-cloud.com:443',
+    logLevel: 'off',
+    captureBody: 'all',
+    errorOnAbortedRequests: false,
+    logUncaughtExceptions: false,
+  })
+  .addFilter((payload) => {
+    return JSON.parse(JSON.stringify(payload, accessTokenReplacer));
+  });
